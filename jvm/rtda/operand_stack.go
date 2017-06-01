641cf255d6f9a7fd95bf4d0916c06b8640624825
张红军 <zhanghongjun228@163.com>
2017-06-01 23:00:26 +0800

    完善操作数栈和局部变量表

--- jvm/rtda/operand_stack.go
+++ jvm/rtda/operand_stack.go
@@ -45,6 +45,50 @@ func (self *OperandStack) PopFloat() float32  {
 	return math.Float32frombits(bits)
 }
 
+//直接压两个int32
+func (self *OperandStack) PushLong(val int64)  {
+	self.slots[self.size].num = int32(val)
+	self.slots[self.size  + 1].num = int32(val >> 32)
+	self.size += 2
+}
+
+//先弹出两个int变量，然后组装成一个long变量
+func (self *OperandStack) PopLong() int64  {
+	self.size -= 2
+	low := uint32(self.slots[self.size].num)
+	high := uint32(self.slots[self.size + 1].num)
+	
+	return int64(high)<< 32 | int64(low)
+}
+
+func (self *OperandStack) PushDouble(val float64) {
+	bits := math.Float64frombits(val)
+	self.PushLong(int64(bits))
+}
+
+func (self *OperandStack) PopDouble() float64 {
+	bits := uint64(self.PopLong())
+	return math.Float64frombits(bits)
+}
+
+//最后是引用类型
+func (self *OperandStack) PushRef(ref *Object)  {
+	self.slots[self.size].ref = ref
+	self.size++
+}
+
+func (self *OperandStack) PopRef() *Object  {
+	self.size--
+	ref := self.slots[self.size].ref
+	self.slots[self.size].ref = nil
+	return ref
+}
+
+
+
+
+
+
 
 
 
