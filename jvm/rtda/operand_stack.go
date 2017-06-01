26c4ca7a6c15b00a19ec47dcd844ac945aaabdad
张红军 <zhanghongjun228@163.com>
2017-06-01 22:41:21 +0800

    添加操作数栈的操作方法

--- jvm/rtda/operand_stack.go
+++ jvm/rtda/operand_stack.go
@@ -1,5 +1,10 @@
 package rtda
 
+import (
+	"math"
+	"os/signal"
+)
+
 //操作数栈
 //操作数栈的大小是编译器已经确定的，所以可以用[]Slot 实现
 // size 用于记录栈顶位置
@@ -17,5 +22,39 @@ func newOperandStack(maxStack uint) *OperandStack  {
 	
 	return nil
 }
+
+func (self *OperandStack) PushInt(val int32)  {
+	self.slots[self.size].num = val
+	self.size++
+}
+
+func (self *OperandStack) PopInt() int32 {
+	self.size--
+	return self.slots[self.size].num
+}
+
+func (self *OperandStack) PushFloat(val float32) {
+	bits := math.Float32bits(val)
+	self.slots[self.size].num = int32(bits)
+	self.size++
+}
+
+func (self *OperandStack) PopFloat() float32  {
+	self.size--
+	bits := uint32(self.slots[self.size].num)
+	return math.Float32frombits(bits)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
